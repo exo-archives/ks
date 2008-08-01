32ae7c4ec5e5feb5499e0cf90ef22bbaa10701fd
Luong Trung Hieu <hieu.luong@exoplatform.com>
2008-08-01 03:35:40 +0000

    add MacSkin and VistaSkin for FAQ

--- eXoApplication/faq/webapp/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ eXoApplication/faq/webapp/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -4,3 +4,17 @@ SkinService.addSkin(
     "/faq/skin/DefaultSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
+
+SkinService.addSkin(
+    "faq/FAQPortlet",
+    "Mac",
+    "/faq/skin/MacSkin/webui/Stylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "faq/FAQPortlet",
+    "Vista",
+    "/faq/skin/VistaSkin/webui/Stylesheet.css",
+    ServletContext
+) ;
