8f33cf377629caa1d21cf577351131eec10d109f
Mai Van Ha <ha.mai@exoplatform.com>
2008-09-06 08:57:48 +0000

    ks-559 Check view permisson for topic/post view by user in many cases

--- eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/popup/UIPageListTopicByUser.java
+++ eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/popup/UIPageListTopicByUser.java
@@ -17,13 +17,16 @@
 package org.exoplatform.forum.webui.popup;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.forum.ForumSessionUtils;
 import org.exoplatform.forum.ForumUtils;
+import org.exoplatform.forum.service.Category;
 import org.exoplatform.forum.service.Forum;
 import org.exoplatform.forum.service.ForumService;
+import org.exoplatform.forum.service.ForumServiceUtils;
 import org.exoplatform.forum.service.JCRPageList;
 import org.exoplatform.forum.service.Topic;
 import org.exoplatform.forum.service.UserProfile;
@@ -35,8 +38,10 @@ import org.exoplatform.forum.webui.UIForumPortlet;
 import org.exoplatform.forum.webui.UITopicDetail;
 import org.exoplatform.forum.webui.UITopicDetailContainer;
 import org.exoplatform.forum.webui.UITopicPoll;
+import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIApplication;
 import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.core.UIContainer;
 import org.exoplatform.webui.event.Event;
@@ -134,6 +139,65 @@ public class UIPageListTopicByUser extends UIContainer{
 			UIPageListTopicByUser uiForm = event.getSource() ;
       String topicId = event.getRequestContext().getRequestParameter(OBJECTID) ;
       Topic topic = uiForm.getTopicById(topicId) ;
+      UIApplication uiApp = uiForm.getAncestorOfType(UIApplication.class) ;
+      String path =  topic.getPath().replaceFirst("/exo:applications/ForumService/", "");
+      String []id = path.split("/") ;
+			boolean isRead = true;
+			Category category = uiForm.forumService.getCategory(ForumSessionUtils.getSystemProvider(), id[0]);
+			if(category == null) {
+				uiApp.addMessage(new ApplicationMessage("UIShowBookMarkForm.msg.link-not-found", null, ApplicationMessage.WARNING)) ;
+				return ;
+			}
+			String[] privateUser = category.getUserPrivate();
+			if(privateUser != null && privateUser.length > 0) {
+				if(privateUser.length ==1 && privateUser[0].equals(" ")){
+					isRead = true;
+				} else {
+					isRead = ForumServiceUtils.hasPermission(privateUser, uiForm.userProfile.getUserId());
+				}
+			}
+			if(isRead) {
+				Forum forum = uiForm.forumService.getForum(ForumSessionUtils.getSystemProvider(),id[0] , id[1] ) ;
+				if(forum == null ){
+					String[] s = new String[]{};
+					uiApp.addMessage(new ApplicationMessage("UIForumPortlet.msg.do-not-permission", s, ApplicationMessage.WARNING)) ;
+					return;
+				}
+				
+				if(uiForm.userProfile.getUserRole() == 0 || 
+						(forum.getModerators() != null && forum.getModerators().length > 0 && 
+								ForumServiceUtils.hasPermission(forum.getModerators(), uiForm.userProfile.getUserId()))) isRead = true;
+				else isRead = false;
+				
+				if(!isRead && !forum.getIsClosed() && !forum.getIsLock()){
+					List<String> listUserPermission = new ArrayList<String>();
+					if (forum.getCreateTopicRole() != null && forum.getCreateTopicRole().length > 0) 
+						listUserPermission.addAll(Arrays.asList(forum.getCreateTopicRole()));
+				
+					if(forum.getViewer() != null && forum.getViewer().length > 0 )
+						listUserPermission.addAll(Arrays.asList(forum.getViewer()));
+					
+					if(ForumServiceUtils.hasPermission(listUserPermission.toArray(new String[]{}), uiForm.userProfile.getUserId())) isRead = true;
+					
+					// check for topic:
+					if(!isRead && topic.getIsActiveByForum() && topic.getIsApproved() && !topic.getIsClosed() && 
+							!topic.getIsLock() && !topic.getIsWaiting()){
+						if((topic.getCanPost().length == 1 && topic.getCanPost()[0].equals(" ")) || 
+								ForumServiceUtils.hasPermission(topic.getCanPost(),uiForm.userProfile.getUserId()) ||
+								(topic.getCanView().length == 1 && topic.getCanView()[0].equals(" ")) ||
+								ForumServiceUtils.hasPermission(topic.getCanView(),uiForm.userProfile.getUserId())) isRead = true;
+						else isRead = false;
+					} else {
+						isRead = false;
+					}
+				}
+			}
+			if(!isRead){
+				String[] s = new String[]{};
+				uiApp.addMessage(new ApplicationMessage("UIForumPortlet.msg.do-not-permission", s, ApplicationMessage.WARNING)) ;
+				return;
+			}
+      
       if(((UIComponent)uiForm.getParent()).getId().equals("UIModeratorManagementForm")) {
       	UIModeratorManagementForm parentComponent = uiForm.getParent();
 				UIPopupContainer popupContainer = parentComponent.getAncestorOfType(UIPopupContainer.class) ;
@@ -144,7 +208,7 @@ public class UIPageListTopicByUser extends UIContainer{
 				event.getRequestContext().addUIComponentToUpdateByAjax(popupAction) ;
       } else {
 	      UIForumPortlet forumPortlet = uiForm.getAncestorOfType(UIForumPortlet.class) ;
-	      String []id = topic.getPath().split("/") ;
+	      id = topic.getPath().split("/") ;
 	      int i = id.length ;
 	      String categoryId = id[i-3];
 	      String forumId = id[i-2] ;
