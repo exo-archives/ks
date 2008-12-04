d4e80842f2fcf33c9ea8ea889c6c4eac1c5755bb
Mai Van Ha <ha.mai@exoplatform.com>
2008-12-04 04:33:37 +0000

    ks-Move question can not be done

--- eXoApplication/faq/service/src/main/java/org/exoplatform/faq/service/impl/JCRDataStorage.java
+++ eXoApplication/faq/service/src/main/java/org/exoplatform/faq/service/impl/JCRDataStorage.java
@@ -272,11 +272,13 @@ public class JCRDataStorage {
 		}
 		
 		// reset link of question before send mail:
-		String path = "";
-		if(!question.getCategoryId().equals("null"))
-			path = getCategoryNodeById(question.getCategoryId(), sProvider).getPath().replace("/exo:applications/faqApp/catetories/", "");
-		path = (question.getLink().substring(0, question.getLink().indexOf("FAQService/") + 11) + path).replace("private", "public");
-		question.setLink(path);
+		if(question.getLink().trim().length() > 0){
+			String path = "";
+			if(!question.getCategoryId().equals("null"))
+				path = getCategoryNodeById(question.getCategoryId(), sProvider).getPath().replace("/exo:applications/faqApp/catetories/", "");
+			path = (question.getLink().substring(0, question.getLink().indexOf("FAQService/") + 11) + path).replace("private", "public");
+			question.setLink(path);
+		}
 		
 		if(faqSetting.getDisplayMode().equals("approved")) {
 			//Send notifycation when add new question in watching category
