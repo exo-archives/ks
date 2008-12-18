b84d47ca5f9694cd5fba73eede432d2d985dbe81
Dung Hoang <dung.hoang@exoplatform.com>
2008-12-18 17:06:32 +0000

    refine slider bar control

--- web/ksResources/src/main/webapp/javascript/eXo/ks/UISliderControl.js
+++ web/ksResources/src/main/webapp/javascript/eXo/ks/UISliderControl.js
@@ -10,24 +10,18 @@ UISliderControl.prototype.findMouseRelativeX = function(cont,evt){
 	return (mouseX - contX);
 };
 
-UISliderControl.prototype.initValue = function(){
-	return ;
-};
-
 UISliderControl.prototype.start = function(obj,evt){
 	var Browser = eXo.core.Browser;
 	this.object = eXo.core.DOMUtil.findFirstDescendantByClass(obj,"div","SliderPointer") ;
 	this.container = obj;
 	this.inputField = eXo.core.DOMUtil.findDescendantsByTagName(obj.parentNode,"input")[0] ;
 	var mouseX = this.findMouseRelativeX(this.container,evt);
-	var width = (mouseX < 7)?14:(mouseX + 7);
-	var props = eXo.webui.UISliderControl.getInitValue(mouseX);
+	var props = eXo.webui.UISliderControl.getValue(mouseX);
 	this.object.style.width = props[0] + "px";
 	this.inputField.value = props[1]*5 ;
 	this.inputField.previousSibling.innerHTML = props[1]*5;
 	document.onmousemove = this.execute;
 	document.onmouseup = this.end;
-	document.title = mouseX ;
 };
 
 UISliderControl.prototype.execute = function(evt){
@@ -46,35 +40,22 @@ UISliderControl.prototype.getValue = function(mouseX){
 	var width = 0;	
 	var value = 0;
 	mouseX = parseInt(mouseX);
-	if (mouseX < 7) {
+	if (mouseX <= 7) {
   	width = 14;
+		value = 0;
   }
-  else if (mouseX > 221){
+  else if((mouseX > 7) && (mouseX <= 200)) {
+ 		width = mouseX + 7;		
+		value = width - 14;
+	}
+	else if((mouseX > 200) && (mouseX < 221)) {
+		width = mouseX + 7;		
+		value = width - 28;
+	}
+	else{
 		width = 228;
 		value = 200;
 	} 
-  else {
- 		width = mouseX + 7;		
-		value = mouseX;
-	}
-	return [width,value];
-};
-
-UISliderControl.prototype.getInitValue = function(mouseX){
-	var width = 0;	
-	var value = 0;
-	mouseX = parseInt(mouseX);
-	if (mouseX < 14) {
-  	width = 14;
-  }
-	else if (mouseX > 214){
-		width = 228;
-		value = 200;
-	} 
-  else {
- 		width = mouseX + 7;		
-		value = mouseX - 14;
-	}
 	return [width,value];
 };
 
@@ -83,18 +64,6 @@ UISliderControl.prototype.end = function(){
 	eXo.webui.UISliderControl.container = null;
 	document.onmousemove = null;
 	document.onmouseup = null;
-};
-
-UISliderControl.prototype.getObjectLeft = function(obj,cont){
-	if(obj.offsetLeft < (-obj.offsetWidth/2)) return -obj.offsetWidth/2 ;
-	else if (obj.offsetLeft > (cont.offsetWidth - (obj.offsetWidth/2))) return cont.offsetWidth - (obj.offsetWidth/2) ;
-	else return 0;
-};
-
-UISliderControl.prototype.isIn = function(evt,cont){
-	var mouseX = this.findMouseRelativeX(cont,evt);
-	if(mouseX < 0 || mouseX > cont.offsetWidth) return false;
-	return true;
 };
 
 eXo.webui.UISliderControl = new UISliderControl();
