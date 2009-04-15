59411a5407d3d5890e87af6e3143e96b02f8120d
Nguyen Quang Hung <hung.nguyen@exoplatform.com>
2009-04-15 02:21:44 +0000

    KS-1379: Create portlets

---
+++ eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/info/UIForumModeratePortlet.java
@@ -0,0 +1,46 @@
+/***************************************************************************
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+package org.exoplatform.forum.info;
+
+import org.exoplatform.webui.application.WebuiApplication;
+import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.UIPortletApplication;
+import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+
+/**
+ * Author : Hung Nguyen Quang
+ *          hung.nguyen@exoplatform.com
+ * Mar 04, 2008
+ */
+
+@ComponentConfig(
+   lifecycle = UIApplicationLifecycle.class,
+   template = "app:/templates/forum/webui/info/UIForumModeratePortlet.gtmpl"
+)
+
+public class UIForumModeratePortlet extends UIPortletApplication {
+	public UIForumModeratePortlet() throws Exception {
+  	
+  }
+  
+  public void processRender(WebuiApplication app, WebuiRequestContext context) throws Exception {    
+   
+    super.processRender(app, context) ;
+  }  
+  
+} 
