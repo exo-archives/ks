9d74d9786d42769bfb94d5bee1d85f52d45c02de
Nguyen Quang Hung <hung.nguyen@exoplatform.com>
2009-07-02 02:31:26 +0000

    KS-1667: Add class to get template for FAQ viewer

---
+++ eXoApplication/faq/webapp/src/main/java/org/exoplatform/faq/webui/FAQResourceResolver.java
@@ -0,0 +1,52 @@
+package org.exoplatform.faq.webui;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.faq.service.FAQService;
+import org.exoplatform.resolver.ResourceResolver;
+
+public class FAQResourceResolver extends ResourceResolver{
+
+	@Override
+	public InputStream getInputStream(String url) throws Exception {
+		ExoContainer container = ExoContainerContext.getCurrentContainer() ;
+		FAQService faqService = (FAQService)container.getComponentInstanceOfType(FAQService.class) ;
+		//get template from faq service
+		byte[] data = new byte[]{} ;
+		return new ByteArrayInputStream(data) ;		
+	}
+
+	@Override
+	public List<InputStream> getInputStreams(String url) throws Exception {
+		ArrayList<InputStream>  inputStreams = new ArrayList<InputStream>(1) ;
+    inputStreams.add(getInputStream(url)) ;
+    return inputStreams ;
+	}
+
+	@Override
+	public URL getResource(String arg0) throws Exception {
+		throw new Exception("This method is not  supported") ;  
+	}
+
+	@Override
+	public String getResourceScheme() {
+		return "jcr:" ;
+	}
+
+	@Override
+	public List<URL> getResources(String arg0) throws Exception {
+		throw new Exception("This method is not  supported") ;  
+	}
+
+	@Override
+	public boolean isModified(String arg0, long arg1) {
+		return false;
+	}
+
+}
