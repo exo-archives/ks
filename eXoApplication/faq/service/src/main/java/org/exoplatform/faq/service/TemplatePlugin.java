4ed9562fe97651714fdc3cdcb7de773ded2caa1d
Nguyen Quang Hung <hung.nguyen@exoplatform.com>
2009-07-02 09:20:48 +0000

    KS-1667: Add plugin for load default template for Viewer portlet

---
+++ eXoApplication/faq/service/src/main/java/org/exoplatform/faq/service/TemplatePlugin.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.faq.service;
+
+import org.exoplatform.container.component.BaseComponentPlugin;
+import org.exoplatform.container.xml.InitParams;
+
+/**
+ * Created by The eXo Platform SARL
+ * Jul 2, 2009, 4:25:54 PM
+ */
+public class TemplatePlugin extends BaseComponentPlugin {
+	private String path_ ;
+
+  @SuppressWarnings("unchecked")
+  public TemplatePlugin(InitParams params) throws Exception {
+  	path_ = params.getValueParam("viewerTemplate").getValue() ;    
+  }
+  public String getPath() { return path_ ;}
+  
+}
