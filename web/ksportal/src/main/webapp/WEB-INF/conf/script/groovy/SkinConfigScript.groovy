593fdb66977ca82a1dc1108a70eba69d5e6c5d28
Tu Duy Vu <tuvd@exoplatform.com>
2009-09-17 06:42:50 +0000

    KS-1797: Demo portal

--- web/ksportal/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/ksportal/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -17,7 +17,32 @@ SkinService.addSkin(
     "web/HomePagePortlet",
     "Mac",
     "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
-    ServletContext
+    ServletContext,
+    true
+) ;
+
+SkinService.addSkin(
+    "web/BannerPortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext,
+    true
+) ;
+
+SkinService.addSkin(
+    "web/BannerPortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext,
+    true
+) ;
+ 
+SkinService.addSkin(
+    "web/BannerPortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext,
+    true
 ) ;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
