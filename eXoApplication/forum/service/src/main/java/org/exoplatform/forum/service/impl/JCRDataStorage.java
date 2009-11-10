a5ad9a3219a084d1cd4c8a5175cace7a719ef532
Nguyen Quang Hung <hung.nguyen@exoplatform.com>
2009-11-10 07:05:27 +0000

    KS-1945: Sort user list alphabetically

--- eXoApplication/forum/service/src/main/java/org/exoplatform/forum/service/impl/JCRDataStorage.java
+++ eXoApplication/forum/service/src/main/java/org/exoplatform/forum/service/impl/JCRDataStorage.java
@@ -2919,8 +2919,15 @@ public class JCRDataStorage {
 
 	public JCRPageList getPageListUserProfile(SessionProvider sProvider) throws Exception {
 		Node userProfileNode = getUserProfileHome(sProvider);
-		NodeIterator iterator = userProfileNode.getNodes();
-		JCRPageList pageList = new ForumPageList(iterator, 10, userProfileNode.getPath(), false);
+		QueryManager qm = userProfileNode.getSession().getWorkspace().getQueryManager();
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("/jcr:root").append(userProfileNode.getPath())
+		.append("/element(*,").append(Utils.USER_PROFILES_TYPE).append(")")
+		.append(" order by @exo:userId ascending");
+		Query query = qm.createQuery(stringBuffer.toString(), Query.XPATH);
+		QueryResult result = query.execute();
+		NodeIterator iter = result.getNodes();			
+		JCRPageList pageList = new ForumPageList(iter, 10, stringBuffer.toString(), true);
 		return pageList;
 	}
 
