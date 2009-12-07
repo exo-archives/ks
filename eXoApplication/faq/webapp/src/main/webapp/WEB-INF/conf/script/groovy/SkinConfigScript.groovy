eda786abd74b3749c67b3cf463e46dc8fc127935
Tu Duy Vu <tuvd@exoplatform.com>
2009-12-07 07:45:10 +0000

    KS-862:  Remove hard coded references to /portal from web\ksportal\src\main\webapp\WEB-INF\conf\script\groovy\SkinConfigScript.groovy

--- eXoApplication/faq/webapp/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ eXoApplication/faq/webapp/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,41 +1,41 @@
 SkinService.addSkin(
     "faq/AnswersPortlet",
     "Default",
-    "/faq/skin/DefaultSkin/webui/Stylesheet.css",
+   "/" + ServletContextName + "/skin/DefaultSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
 
 SkinService.addSkin(
     "faq/AnswersPortlet",
     "Mac",
-    "/faq/skin/MacSkin/webui/Stylesheet.css",
+   "/" + ServletContextName + "/skin/MacSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
 
 SkinService.addSkin(
     "faq/AnswersPortlet",
     "Vista",
-    "/faq/skin/VistaSkin/webui/Stylesheet.css",
+   "/" + ServletContextName + "/skin/VistaSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
 
 SkinService.addSkin(
     "faq/FAQPortlet",
     "Default",
-    "/faq/skin/DefaultSkin/webui/Stylesheet.css",
+   "/" + ServletContextName + "/skin/DefaultSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
 
 SkinService.addSkin(
     "faq/FAQPortlet",
     "Mac",
-    "/faq/skin/MacSkin/webui/Stylesheet.css",
+   "/" + ServletContextName + "/skin/MacSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
 
 SkinService.addSkin(
     "faq/FAQPortlet",
     "Vista",
-    "/faq/skin/VistaSkin/webui/Stylesheet.css",
+   "/" + ServletContextName + "/skin/VistaSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
--- eXoApplication/forum/webapp/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ eXoApplication/forum/webapp/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,6 +1,6 @@
 SkinService.addSkin(
     "forum/ForumPortlet",
     "Default",
-    "/forum/skin/DefaultSkin/webui/Stylesheet.css",
+    "/" + ServletContextName + "/skin/DefaultSkin/webui/Stylesheet.css",
     ServletContext
 ) ;
