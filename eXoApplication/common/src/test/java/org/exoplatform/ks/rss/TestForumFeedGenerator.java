b50d096a41dd442c2519cf66157419b238b0a416
Patrice Lamarque <patrice.lamarque@exoplatform.com>
2010-01-02 10:35:58 +0000

    placeholder for unit test

---
+++ eXoApplication/common/src/test/java/org/exoplatform/ks/rss/TestForumFeedGenerator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.ks.rss;
+
+import org.exoplatform.ks.common.jcr.KSDataLocation;
+import org.exoplatform.ks.test.ConfigurationUnit;
+import org.exoplatform.ks.test.ConfiguredBy;
+import org.exoplatform.ks.test.ContainerScope;
+import org.exoplatform.ks.test.jcr.AbstractJCRTestCase;
+
+/**
+ * @author <a href="mailto:patrice.lamarque@exoplatform.com">Patrice Lamarque</a>
+ * @version $Revision$
+ */
+@ConfiguredBy({@ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/jcr/jcr-configuration.xml")})
+public class TestForumFeedGenerator extends AbstractJCRTestCase {
+
+  ForumFeedGenerator generator = null;
+  
+  protected void setUp() throws Exception {
+    super.setUp();
+    KSDataLocation locator = new KSDataLocation(getRepository(), getWorkspace());
+    generator = new ForumFeedGenerator(locator);
+  }
+ 
+
+
+}
