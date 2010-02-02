714c55195e87471eeed6d3236574815a758bf07a
Patrice Lamarque <patrice.lamarque@exoplatform.com>
2010-02-02 16:30:46 +0000

    KS-2188 fck and comet are now taken from platform

--- packaging/module/src/main/javascript/ks.packaging.module.js
+++ packaging/module/src/main/javascript/ks.packaging.module.js
@@ -16,16 +16,16 @@ function getModule(params)
  
   	// COMET (required by KS)
   	// TODO, should be passed in params and have its own module .js definition 
-  var cometVersion = "${org.exoplatform.comet.version}";
+  var cometVersion = "${org.exoplatform.platform.version}";
   module.comet = {};
 
     
   module.comet.cometd =
-	new Project("org.exoplatform.comet", "exo-comet-webapp", "war", cometVersion).
+	new Project("org.exoplatform.platform", "exo.platform.commons.comet.webapp", "war", cometVersion).
     addDependency(new Project("org.mortbay.jetty", "cometd-bayeux", "jar", "6.1.11")).
 	addDependency(new Project("org.mortbay.jetty", "jetty-util", "jar", "6.1.11")).
 	addDependency(new Project("org.mortbay.jetty", "cometd-api", "jar", "0.9.20080221")).
-	addDependency(new Project("org.exoplatform.comet", "exo-comet-service", "jar", cometVersion));  	
+	addDependency(new Project("org.exoplatform.platform", "exo.platform.commons.comet.service", "jar", cometVersion));  	
 	module.comet.cometd.deployName = "cometd";
   // KS
 
