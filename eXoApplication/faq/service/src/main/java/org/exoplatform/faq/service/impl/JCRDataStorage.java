b7275cb9faa29f926199f9e968d44dd89c2e325e
Tu Duy Vu <tuvd@exoplatform.com>
2010-02-23 08:07:41 +0000

    KS-2253:  Question with attachment disappears after move

--- eXoApplication/faq/service/src/main/java/org/exoplatform/faq/service/impl/JCRDataStorage.java
+++ eXoApplication/faq/service/src/main/java/org/exoplatform/faq/service/impl/JCRDataStorage.java
@@ -1577,7 +1577,7 @@ public class JCRDataStorage implements DataStorage {
 					while(iter.hasNext()) {
 						attNode = iter.nextNode() ;
 						if(attNode.isNodeType("exo:faqAttachment")) {
-							attNode.setProperty("exo:categoryId", catId) ;
+							attNode.getNode("jcr:content").setProperty("exo:categoryId", catId) ;
 						}
 					}
 					updateAnswers(questionNode, catId) ;
