0edd79a2c6e766825e206facd003cb7aa2acc574
Tu Duy Vu <tuvd@exoplatform.com>
2010-03-16 11:07:55 +0000

    KS-2244: Forun : the discussion windows must be resized

--- eXoApplication/common/src/main/java/org/exoplatform/ks/common/webui/AbstractPopupAction.java
+++ eXoApplication/common/src/main/java/org/exoplatform/ks/common/webui/AbstractPopupAction.java
@@ -32,79 +32,86 @@ import org.exoplatform.webui.core.lifecycle.Lifecycle;
  */
 @ComponentConfig(lifecycle = Lifecycle.class)
 public abstract class AbstractPopupAction extends UIContainer {
-  
-  public AbstractPopupAction() throws Exception {
-     addChild(createUIComponent(UIPopupWindow.class, null, null).setRendered(false));
-  }
-  
-  @Override
-  public void processRender(WebuiRequestContext context) throws Exception {
-    context.getWriter()
-           .append("<span class=\"")
-           .append(getId())
-           .append(" UIKSPopupAction\" id=\"")
-           .append(getId())
-           .append("\">");
-    renderChildren(context);
-    context.getWriter().append("</span>");
-    afterProcessRender(context);
-  }
 
-  protected abstract void afterProcessRender(WebuiRequestContext context);
+	public AbstractPopupAction() throws Exception {
+		UIPopupWindow popupWindow = createUIComponent(UIPopupWindow.class, null, null).setRendered(false);
+		popupWindow.setResizable(true);
+		addChild(popupWindow);
+	}
 
-  public <T extends UIComponent> T activate(Class<T> type, int width) throws Exception {
-    return activate(type, null, width, 0);
-  }
+	@Override
+	public void processRender(WebuiRequestContext context) throws Exception {
+		context.getWriter().append("<span class=\"").append(getId()).append(
+		    " UIKSPopupAction\" id=\"").append(getId()).append("\">");
+		renderChildren(context);
+		context.getWriter().append("</span>");
+		afterProcessRender(context);
+	}
 
-  public <T extends UIComponent> T activate(Class<T> type, String configId, int width, int height) throws Exception {
-    T comp = createUIComponent(type, configId, null);
-    activate(comp, width, height);
-    return comp;
-  }
+	protected abstract void afterProcessRender(WebuiRequestContext context);
 
-  public void activate(UIComponent uiComponent, int width, int height) throws Exception {
-    activate(uiComponent, width, height, true);
-  }
+	public <T extends UIComponent> T activate(Class<T> type, int width)
+	    throws Exception {
+		return activate(type, null, width, 0);
+	}
 
-  public void activate(UIComponent uiComponent, int width, int height, boolean isResizeable) throws Exception {
-    UIPopupWindow popup = getChild(UIPopupWindow.class);
-    popup.setUIComponent(uiComponent);
-    ((UIPopupComponent) uiComponent).activate();
-    popup.setWindowSize(width, height);
-    popup.setRendered(true);
-    popup.setShow(true);
-    popup.setResizable(isResizeable);
-  }
+	public <T extends UIComponent> T activate(Class<T> type, String configId,
+	    int width, int height) throws Exception {
+		T comp = createUIComponent(type, configId, null);
+		activate(comp, width, height);
+		return comp;
+	}
 
-  public void deActivate() throws Exception {
-    UIPopupWindow popup = getChild(UIPopupWindow.class);
-    if (popup.getUIComponent() != null)
-      ((UIPopupComponent) popup.getUIComponent()).deActivate();
-    popup.setUIComponent(null);
-    popup.setRendered(false);
-  }
+	public void activate(UIComponent uiComponent, int width, int height)
+	    throws Exception {
+		activate(uiComponent, width, height, true);
+	}
 
-  public void cancelPopupAction() throws Exception {
-    deActivate();
-    WebuiRequestContext context = RequestContext.getCurrentInstance();
-    context.addUIComponentToUpdateByAjax(this);
-  }
+	public void activate(UIComponent uiComponent, int width, int height,
+	    boolean isResizeable) throws Exception {
+		UIPopupWindow popup = getChild(UIPopupWindow.class);
+		popup.setUIComponent(uiComponent);
+		((UIPopupComponent) uiComponent).activate();
+		popup.setWindowSize(width, height);
+		popup.setRendered(true);
+		popup.setShow(true);
+		popup.setResizable(isResizeable);
+	}
 
-  public final UIPopupContainer prepareForNewForm() throws Exception {
-    UIPopupContainer popupContainer = createUIComponent(UIPopupContainer.class, null, null);
-  	String id = "UIPopup";  
-  	try {
-  		id = getParent().getId();
-  		if(!id.contains("Portlet") || id.contains("Container")){
-  			id = getAncestorOfType(UIPortletApplication.class).getId().replaceFirst("Portlet", "");
-  			setId(id+"ChildPopupAction");
-  			getChild(UIPopupWindow.class).setId(id + "ChildPopupWindow") ;
-  		}
-    } catch (Exception e) {}
-    // prepare new child popup
-    AbstractPopupAction childPopupAction  = popupContainer.addChild(getClass(), null, getName() + "ChildPopupAction").setRendered(true);
-    childPopupAction.getChild(UIPopupWindow.class).setId(getName() + "ChildPopupWindow") ;
-    return popupContainer;
-  }
+	public void deActivate() throws Exception {
+		UIPopupWindow popup = getChild(UIPopupWindow.class);
+		if (popup.getUIComponent() != null)
+			((UIPopupComponent) popup.getUIComponent()).deActivate();
+		popup.setUIComponent(null);
+		popup.setRendered(false);
+	}
+
+	public void cancelPopupAction() throws Exception {
+		deActivate();
+		WebuiRequestContext context = RequestContext.getCurrentInstance();
+		context.addUIComponentToUpdateByAjax(this);
+	}
+
+	public final UIPopupContainer prepareForNewForm() throws Exception {
+		UIPopupContainer popupContainer = createUIComponent(UIPopupContainer.class,
+		    null, null);
+		String id = "UIPopup";
+		try {
+			id = getParent().getId();
+			if (!id.contains("Portlet") || id.contains("Container")) {
+				id = getAncestorOfType(UIPortletApplication.class).getId()
+				    .replaceFirst("Portlet", "");
+				setId(id + "ChildPopupAction");
+				getChild(UIPopupWindow.class).setId(id + "ChildPopupWindow");
+			}
+		} catch (Exception e) {
+		}
+		// prepare new child popup
+		AbstractPopupAction childPopupAction = popupContainer.addChild(getClass(),
+		    null, getName() + "ChildPopupAction").setRendered(true);
+		childPopupAction.getChild(UIPopupWindow.class).setId(
+		    getName() + "ChildPopupWindow");
+		return popupContainer;
+	}
 
 }
