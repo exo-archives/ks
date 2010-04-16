5a13dd9b989c80b54f21d1d8853ffd250a9d2294
Tuan  Pham <tuan.pham@exoplatform.com>
2010-04-16 04:10:40 +0000

    ks-2334 : sho hidden forum config

--- eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/UICategory.java
+++ eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/UICategory.java
@@ -182,6 +182,7 @@ public class UICategory extends BaseForumForm	{
 
 	
 	private boolean isShowForum(String id) {
+	  if(this.getAncestorOfType(UIForumPortlet.class).getInvisibleCategories().isEmpty()) return true;
 		List<String> list = new ArrayList<String>();
 		list.addAll(this.getAncestorOfType(UIForumPortlet.class).getInvisibleForums());
 		return (list.contains(id))?true:false;
