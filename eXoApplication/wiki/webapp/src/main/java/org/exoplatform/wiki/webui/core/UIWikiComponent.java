a0a226a1515f85399da8ebbdb9ba6a8f59aaa2d9
Hieu Trung Lai <hieu.lai@exoplatform.com>
2010-09-01 06:29:26 +0000

    KS-2707: Wiki - Rendering UI component when switch Mode

---
+++ eXoApplication/wiki/webapp/src/main/java/org/exoplatform/wiki/webui/core/UIWikiComponent.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2003-2010 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.wiki.webui.core;
+
+import java.util.List;
+
+import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.core.UIComponent;
+import org.exoplatform.wiki.webui.UIWikiPortlet;
+import org.exoplatform.wiki.webui.WikiMode;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : Lai Trung Hieu
+ *          hieu.lai@exoplatform.com
+ * Aug 31, 2010  
+ */
+public class UIWikiComponent extends UIComponent {
+  public List<WikiMode> accept_Modes;
+
+  @Override
+  public void processRender(WebuiRequestContext context) throws Exception {
+    // TODO Auto-generated method stub
+    WikiMode currentMode = getCurrentMode();    
+    if (currentMode != null && accept_Modes.contains(currentMode))
+      super.processRender(context);
+  }
+
+  public WikiMode getCurrentMode() {
+    return getAncestorOfType(UIWikiPortlet.class).getWikiMode();
+  }
+}
