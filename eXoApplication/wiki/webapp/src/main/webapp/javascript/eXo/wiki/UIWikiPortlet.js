bf21be116c8a163a851c36e79608d682bf906d18
Hieu Trung Lai <hieu.lai@exoplatform.com>
2010-09-29 02:22:09 +0000

    KS-2740: Wiki - Search Context Menu

--- eXoApplication/wiki/webapp/src/main/webapp/javascript/eXo/wiki/UIWikiPortlet.js
+++ eXoApplication/wiki/webapp/src/main/webapp/javascript/eXo/wiki/UIWikiPortlet.js
@@ -40,11 +40,11 @@ UIWikiPortlet.prototype.init = function(portletId,linkId){
 
 UIWikiPortlet.prototype.onMouseUp = function(evt){
   var evt = evt || window.event; 
-  var target = evt.target || evt.srcElement;  
+  var target = evt.target || evt.srcElement;   
+  if (evt.button==2) return;
   var searchPopup= eXo.core.DOMUtil.findFirstDescendantByClass(this.wikiportlet,"div","SearchPopup");
   if (searchPopup)
-    searchPopup.style.display='none';  
-  if (evt.button==2) return;
+    searchPopup.style.display='none';
   if (target.tagName=="A"||(target.tagName=="INPUT" && target.type=="button")||
       target.tagName=="SELECT"|| target.tagName=="DIV"&& target.className.indexOf("RefreshModeTarget")>0){    
     eXo.wiki.UIWikiPortlet.changeMode(evt); 
