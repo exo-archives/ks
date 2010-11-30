dc41c3ddb16ce5b69aa0454a271d3f45a8b63d28
Tu Duy Vu <tuvd@exoplatform.com>
2010-11-30 06:46:50 +0000

    KS-2895: Permission tab: existing role/group is not listed after select user

--- eXoApplication/common/src/main/java/org/exoplatform/ks/common/webui/UISelectComponent.java
+++ eXoApplication/common/src/main/java/org/exoplatform/ks/common/webui/UISelectComponent.java
@@ -8,20 +8,27 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see<http://www.gnu.org/licenses/>.
  **/
-package org.exoplatform.faq.webui.popup;
+package org.exoplatform.ks.common.webui;
+
+import org.exoplatform.webui.core.UIComponent;
 
 /**
  * Created by The eXo Platform SARL
  * Author : Pham Tuan
- *          tuan.pham@exoplatform.com
- * Aug 29, 2007  
+ *					tuan.pham@exoplatform.com
+ * Aug 29, 2007	
  */
-public interface UISelector {
-  public void updateSelect(String selectField, String value) throws Exception ;
+public interface UISelectComponent {
+	final public static String TYPE_USER = "0".intern() ;
+	final public static String TYPE_MEMBERSHIP = "1".intern() ;
+	final public static String TYPE_GROUP = "2".intern() ;
+	final public static String TREE_GROUP_ID = "UITreeGroupSelector";
+	final public static String BREADCUMB_GROUP_ID = "BreadcumbGroupSelector";
+	public void setComponent(UIComponent uicomponent, String[] initParams) ;
 }
--- eXoApplication/common/src/main/java/org/exoplatform/ks/common/webui/UISelector.java
+++ eXoApplication/common/src/main/java/org/exoplatform/ks/common/webui/UISelector.java
@@ -8,20 +8,20 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see<http://www.gnu.org/licenses/>.
  **/
-package org.exoplatform.forum.webui.popup;
+package org.exoplatform.ks.common.webui;
 
 /**
  * Created by The eXo Platform SARL
  * Author : Pham Tuan
- *          tuan.pham@exoplatform.com
- * Aug 29, 2007  
+ *					tuan.pham@exoplatform.com
+ * Aug 29, 2007	
  */
 public interface UISelector {
-  public void updateSelect(String selectField, String value) throws Exception ;
+	public void updateSelect(String selectField, String value) throws Exception ;
 }
