e94a380e6db88ce5232f17eef47a86c964a033e8
Quang Thanh Le <quang.le@exoplatform.com>
2011-01-27 04:53:50 +0000

    KS-2966: [wiki] Related Pages     - implement UI.

--- eXoApplication/wiki/webapp/src/main/java/org/exoplatform/wiki/webui/UIWikiPageInfo.java
+++ eXoApplication/wiki/webapp/src/main/java/org/exoplatform/wiki/webui/UIWikiPageInfo.java
@@ -1,6 +1,5 @@
 package org.exoplatform.wiki.webui;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,8 +9,13 @@ import java.util.List;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.web.application.ApplicationMessage;
+import org.exoplatform.web.application.Parameter;
+import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIApplication;
+import org.exoplatform.webui.core.UIPopupContainer;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.wiki.chromattic.ext.ntdef.NTVersion;
@@ -20,25 +24,44 @@ import org.exoplatform.wiki.commons.VersionNameComparatorDesc;
 import org.exoplatform.wiki.mow.api.Page;
 import org.exoplatform.wiki.mow.core.api.wiki.PageImpl;
 import org.exoplatform.wiki.rendering.RenderingService;
+import org.exoplatform.wiki.service.BreadcrumbData;
 import org.exoplatform.wiki.service.WikiPageParams;
+import org.exoplatform.wiki.service.WikiService;
+import org.exoplatform.wiki.webui.UIWikiPortlet.PopupLevel;
 import org.exoplatform.wiki.webui.core.UIWikiContainer;
+import org.exoplatform.wiki.webui.popup.UIWikiSelectPageForm;
 import org.xwiki.rendering.syntax.Syntax;
 
-@ComponentConfig(
-       template = "app:/templates/wiki/webui/UIWikiPageInfo.gtmpl",
-       events = {
-           @EventConfig(listeners = UIWikiPageInfo.ViewRevisionActionListener.class)
-       }
-)
+@ComponentConfig(template = "app:/templates/wiki/webui/UIWikiPageInfo.gtmpl", events = {
+    @EventConfig(listeners = UIWikiPageInfo.ViewRevisionActionListener.class),
+    @EventConfig(listeners = UIWikiPageInfo.AddRelatedPageActionListener.class),
+    @EventConfig(listeners = UIWikiPageInfo.RemoveRelatedPageActionListener.class, confirm = "UIWikiPageInfo.msg.confirm-remove-rpage") })
 public class UIWikiPageInfo extends UIWikiContainer {
-  private static final Log log = ExoLogger.getLogger(UIWikiPageInfo.class);
+  private static final Log log                     = ExoLogger.getLogger(UIWikiPageInfo.class);
+
   private static final int NUMBER_OF_SHOWN_CHANGES = 5;
-  
+
+  private UIWikiBreadCrumb breadcrumb;
+
   public UIWikiPageInfo() throws Exception {
     super();
     this.accept_Modes = Arrays.asList(new WikiMode[] { WikiMode.PAGEINFO });
+    breadcrumb = addChild(UIWikiBreadCrumb.class, null, "UIWikiBreadCrumb_PageInfo");
+    breadcrumb.setLink(false);
   }
 
+  public List<BreadcrumbData> getBreadcrumbDatas(Page page) {
+    WikiService service = getApplicationComponent(WikiService.class);
+    WikiPageParams params = org.exoplatform.wiki.utils.Utils.getWikiPageParams(page);
+    try {
+      return service.getBreadcumb(params.getType(), params.getOwner(), params.getPageId());
+    } catch (Exception e) {
+      if (log.isWarnEnabled()) {
+        log.warn(String.format("can not load BreadcrumbData for page [%s]", page.getName()), e);
+      }
+      return new ArrayList<BreadcrumbData>();
+    }
+  }
 
   List<NTVersion> getVersionList(Page page) {
     List<NTVersion> versions = new ArrayList<NTVersion>();
@@ -52,7 +75,9 @@ public class UIWikiPageInfo extends UIWikiContainer {
         }
       }
       Collections.sort(versions, new VersionNameComparatorDesc());
-      return versions.subList(0, versions.size() > NUMBER_OF_SHOWN_CHANGES ? NUMBER_OF_SHOWN_CHANGES : versions.size());
+      return versions.subList(0,
+                              versions.size() > NUMBER_OF_SHOWN_CHANGES ? NUMBER_OF_SHOWN_CHANGES
+                                                                       : versions.size());
     } catch (Exception e) {
       if (log.isWarnEnabled()) {
         log.warn(String.format("getting version list of page %s failed", page.getName()), e);
@@ -60,28 +85,93 @@ public class UIWikiPageInfo extends UIWikiContainer {
     }
     return versions;
   }
-  
-  
+
+  /**
+   * get ajax link of {@link RemoveRelatedPageActionListener}
+   * 
+   * @param wikiParams
+   * @return
+   */
+  String getRemovePageActionLink(WikiPageParams wikiParams) {
+    Parameter[] params = new Parameter[] {
+        new Parameter(RemoveRelatedPageActionListener.WIKI_TYPE, wikiParams.getType()),
+        new Parameter(RemoveRelatedPageActionListener.PAGE_OWNER, wikiParams.getOwner()),
+        new Parameter(RemoveRelatedPageActionListener.PAGE_ID, wikiParams.getPageId()) };
+    try {
+      return event("RemoveRelatedPage", null, params);
+    } catch (Exception e) {
+      if (log.isWarnEnabled())
+        log.warn("getting Remove related page failed", e);
+      return "";
+    }
+  }
+
   public Page getCurrentPage() throws Exception {
     return Utils.getCurrentWikiPage();
   }
-  
+
   String getPageLink(Page page) throws Exception {
     WikiPageParams params = org.exoplatform.wiki.utils.Utils.getWikiPageParams(page);
     return Utils.getURLFromParams(params);
   }
-  
+
   String renderHierarchy() throws Exception {
     RenderingService renderingService = (RenderingService) PortalContainer.getComponent(RenderingService.class);
     Utils.setUpWikiContext(getAncestorOfType(UIWikiPortlet.class), renderingService);
-    return renderingService.render("{{pagetree /}}", Syntax.XWIKI_2_0.toIdString(), Syntax.XHTML_1_0.toIdString(), false);
+    return renderingService.render("{{pagetree /}}",
+                                   Syntax.XWIKI_2_0.toIdString(),
+                                   Syntax.XHTML_1_0.toIdString(),
+                                   false);
   }
-  
+
+  static public class AddRelatedPageActionListener extends EventListener<UIWikiPageInfo> {
+    @Override
+    public void execute(Event<UIWikiPageInfo> event) throws Exception {
+      UIWikiPageInfo uicomponent = event.getSource();
+      UIWikiPortlet wikiPortlet = uicomponent.getAncestorOfType(UIWikiPortlet.class);
+      UIPopupContainer popupContainer = wikiPortlet.getPopupContainer(PopupLevel.L1);
+      UIWikiSelectPageForm selectPageForm = popupContainer.activate(UIWikiSelectPageForm.class, 600);
+      selectPageForm.addUpdatedComponent(uicomponent);
+      event.getRequestContext().addUIComponentToUpdateByAjax(popupContainer);
+    }
+  }
+
+  static public class RemoveRelatedPageActionListener extends EventListener<UIWikiPageInfo> {
+
+    static final String WIKI_TYPE  = "wikitype";
+
+    static final String PAGE_OWNER = "owner";
+
+    static final String PAGE_ID    = "pageid";
+
+    @Override
+    public void execute(Event<UIWikiPageInfo> event) throws Exception {
+      WebuiRequestContext requestContext = event.getRequestContext();
+      UIWikiPageInfo uicomponent = event.getSource();
+      String wikiType = requestContext.getRequestParameter(WIKI_TYPE);
+      String owner = requestContext.getRequestParameter(PAGE_OWNER);
+      String pageId = requestContext.getRequestParameter(PAGE_ID);
+      try {
+        WikiPageParams relatedPageParams = new WikiPageParams(wikiType, owner, pageId);
+        WikiService service = uicomponent.getApplicationComponent(WikiService.class);
+        service.removeRelatedPage(Utils.getCurrentWikiPageParams(), relatedPageParams);
+      } catch (Exception e) {
+        if (log.isWarnEnabled()) log.warn(String.format("can not remove related page [%s]", pageId), e);
+        UIApplication application = uicomponent.getAncestorOfType(UIApplication.class);
+        application.addMessage(new ApplicationMessage("can not remove this page", null, ApplicationMessage.WARNING));
+        requestContext.addUIComponentToUpdateByAjax(application);
+        
+      }
+      requestContext.addUIComponentToUpdateByAjax(uicomponent);
+    }
+
+  }
+
   static public class ViewRevisionActionListener extends EventListener<UIWikiPageInfo> {
     @Override
     public void execute(Event<UIWikiPageInfo> event) throws Exception {
       UIWikiHistorySpaceArea.viewRevision(event);
     }
   }
-  
+
 }
---
+++ eXoApplication/wiki/webapp/src/main/java/org/exoplatform/wiki/webui/popup/UIWikiSelectPageForm.java
@@ -0,0 +1,153 @@
+package org.exoplatform.wiki.webui.popup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.web.application.ApplicationMessage;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIApplication;
+import org.exoplatform.webui.core.UIComponent;
+import org.exoplatform.webui.core.UIPopupComponent;
+import org.exoplatform.webui.core.UIPopupContainer;
+import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
+import org.exoplatform.webui.event.Event;
+import org.exoplatform.webui.event.EventListener;
+import org.exoplatform.webui.form.UIForm;
+import org.exoplatform.wiki.commons.Utils;
+import org.exoplatform.wiki.resolver.TitleResolver;
+import org.exoplatform.wiki.service.WikiPageParams;
+import org.exoplatform.wiki.service.WikiService;
+import org.exoplatform.wiki.tree.TreeNode;
+import org.exoplatform.wiki.tree.TreeNode.TREETYPE;
+import org.exoplatform.wiki.tree.utils.TreeUtils;
+import org.exoplatform.wiki.webui.UIWikiEmptyAjaxBlock;
+import org.exoplatform.wiki.webui.UIWikiPortlet;
+import org.exoplatform.wiki.webui.UIWikiPortlet.PopupLevel;
+import org.exoplatform.wiki.webui.tree.EventUIComponent;
+import org.exoplatform.wiki.webui.tree.UITreeExplorer;
+import org.exoplatform.wiki.webui.tree.EventUIComponent.EVENTTYPE;
+
+@ComponentConfig(
+    lifecycle = UIFormLifecycle.class,
+    template = "/templates/wiki/webui/popup/UIWikiSelectPageForm.gtmpl",
+    events = {
+        @EventConfig(listeners = UIWikiSelectPageForm.SetCurrentPageActionListener.class),
+        @EventConfig(listeners = UIWikiSelectPageForm.SelectPageActionListener.class),
+        @EventConfig(listeners = UIWikiSelectPageForm.CancelActionListener.class)
+    }
+)
+public class UIWikiSelectPageForm extends UIForm implements UIPopupComponent {
+  private static final Log log                     = ExoLogger.getLogger(UIWikiSelectPageForm.class);
+  
+  public static final String FORM_ID = "UIWikiSelectPageForm";
+  
+  private String currentNodeValue = ""; 
+  
+  public static final String UI_TREE_ID = "UIPageTree";
+  
+  public UIWikiSelectPageForm() throws Exception {
+    setId(FORM_ID);
+    UITreeExplorer uiTree = addChild(UITreeExplorer.class, null, UI_TREE_ID);
+    EventUIComponent eventComponent = new EventUIComponent(FORM_ID,
+                                                           "SetCurrentPage",
+                                                           EVENTTYPE.EVENT);
+    StringBuilder initURLSb = new StringBuilder(Utils.getCurrentRestURL());
+    initURLSb.append("/wiki/tree/").append(TREETYPE.ALL.toString());
+    StringBuilder childrenURLSb = new StringBuilder(Utils.getCurrentRestURL());
+    childrenURLSb.append("/wiki/tree/").append(TREETYPE.CHILDREN.toString());
+    uiTree.init(initURLSb.toString(), childrenURLSb.toString(), getInitParam(), eventComponent);
+  }
+  /**
+   * list of ui component needed to updated when form is submitted.
+   */
+  private List<UIComponent> updatedComponents = new ArrayList<UIComponent>();
+  
+  public void addUpdatedComponent(UIComponent component) {
+    updatedComponents.add(component);
+  }
+  
+  public void removeUpdatedComponent(UIComponent component) {
+    updatedComponents.remove(component);
+  }
+  
+  private String getInitParam() throws Exception {
+    StringBuilder sb = new StringBuilder();
+    String currentPath = Utils.getCurrentWikiPagePath().replaceAll("/", ".");
+    sb.append("?")
+      .append(TreeNode.PATH)
+      .append("=")
+      .append(currentPath)
+      .append("&")
+      .append(TreeNode.CURRENT_PATH)
+      .append("=")
+      .append(currentPath);
+    return sb.toString();
+  }
+  
+  @Override
+  public void activate() throws Exception {
+    
+  }
+  
+  
+  
+  @Override
+  public void deActivate() throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+  
+  static public class SetCurrentPageActionListener extends EventListener<UIWikiSelectPageForm> {
+
+    @Override
+    public void execute(Event<UIWikiSelectPageForm> event) throws Exception {
+      UIWikiSelectPageForm uiform = event.getSource();
+      UIWikiEmptyAjaxBlock emptyBlock = uiform.getAncestorOfType(UIWikiPortlet.class).getChild(UIWikiEmptyAjaxBlock.class);
+      String param = event.getRequestContext().getRequestParameter(OBJECTID);
+      if (param != null) uiform.currentNodeValue = param;
+      event.getRequestContext().addUIComponentToUpdateByAjax(emptyBlock);
+    }
+  }
+  
+  static public class SelectPageActionListener extends EventListener<UIWikiSelectPageForm> {
+
+    @Override
+    public void execute(Event<UIWikiSelectPageForm> event) throws Exception {
+      UIWikiSelectPageForm uiform = event.getSource();
+      UIWikiPortlet wikiPortlet = uiform.getAncestorOfType(UIWikiPortlet.class);
+      try {
+        if (uiform.currentNodeValue.length() > 0) {
+          String currentNodeValue = TitleResolver.getId(uiform.currentNodeValue, false);
+          WikiPageParams params = TreeUtils.getPageParamsFromPath(currentNodeValue);
+
+          WikiService service = uiform.getApplicationComponent(WikiService.class);
+          service.addRelatedPage(Utils.getCurrentWikiPageParams(), params);
+        }
+      } catch (Exception e) {
+         if (log.isWarnEnabled()) log.warn("can not execute 'SelectPage' action", e);
+      }
+      UIPopupContainer popupContainer = wikiPortlet.getPopupContainer(PopupLevel.L1);
+      popupContainer.cancelPopupAction();
+      event.getRequestContext().addUIComponentToUpdateByAjax(popupContainer);
+      for (UIComponent c : uiform.updatedComponents) {
+        event.getRequestContext().addUIComponentToUpdateByAjax(c);
+      }
+    }
+  }
+  
+  static public class CancelActionListener extends EventListener<UIWikiSelectPageForm> {
+
+    @Override
+    public void execute(Event<UIWikiSelectPageForm> event) throws Exception {
+      UIWikiSelectPageForm uiform = event.getSource();
+      UIWikiPortlet wikiPortlet = uiform.getAncestorOfType(UIWikiPortlet.class);
+      UIPopupContainer popupContainer = wikiPortlet.getPopupContainer(PopupLevel.L1);
+      popupContainer.cancelPopupAction();
+      event.getRequestContext().addUIComponentToUpdateByAjax(popupContainer);
+    }
+  }
+  
+}
