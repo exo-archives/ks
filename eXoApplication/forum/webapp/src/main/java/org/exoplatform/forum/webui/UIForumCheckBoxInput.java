7527d6c0fbccc68b75dcc0ad254313a7452b7a09
Tu Duy Vu <tuvd@exoplatform.com>
2011-03-18 11:38:23 +0000

    KS-3164: Error when vote in poll with multi choice

--- eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/UIForumCheckBoxInput.java
+++ eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/UIForumCheckBoxInput.java
@@ -19,9 +19,8 @@ package org.exoplatform.forum.webui;
 import java.io.Writer;
 
 import org.exoplatform.webui.application.WebuiRequestContext;
-import org.exoplatform.webui.form.UIForm;
+import org.exoplatform.webui.form.UIFormCheckBoxInput;
 import org.exoplatform.webui.form.UIFormInput;
-import org.exoplatform.webui.form.UIFormInputBase;
 
 /**
  * Created by The eXo Platform SAS
@@ -29,7 +28,7 @@ import org.exoplatform.webui.form.UIFormInputBase;
  *					tu.duy@exoplatform.com
  * 24 Mar 2008, 08:00:59
  */
-public class UIForumCheckBoxInput<T> extends UIFormInputBase<T>{
+public class UIForumCheckBoxInput<T> extends UIFormCheckBoxInput<T>{
 	 /**
 	 * Whether this checkbox is checked
 	 */
@@ -37,8 +36,6 @@ public class UIForumCheckBoxInput<T> extends UIFormInputBase<T>{
 	/**
 	 * A javascript expression that will be fired when the value changes (JS onChange event)
 	 */
-	private String onchange_;
-	private String componentEvent_ = null;
 
 	@SuppressWarnings("unchecked")
 	public UIForumCheckBoxInput(String name, String bindingExpression, T value) {
@@ -48,7 +45,6 @@ public class UIForumCheckBoxInput<T> extends UIFormInputBase<T>{
 		setId(name);
 	}
 	
-	@SuppressWarnings("unchecked")
 	public UIFormInput setValue(T value){
 		if(value == null) return super.setValue(value);
 		if(value instanceof Boolean){
@@ -60,49 +56,34 @@ public class UIForumCheckBoxInput<T> extends UIFormInputBase<T>{
 		return super.setValue(value);
 	}
 	
-	public void setOnChange(String onchange){ onchange_ = onchange; }	
- 
-	public void setComponentEvent(String com){ componentEvent_ = com; }
+	final public boolean isCheckedBox() { 
+	  checked = isChecked();
+	  return checked; 
+	}	
 	
-	public void setOnChange(String event, String com){
-		this.onchange_ = event; 
-		this.componentEvent_ = com;
-	} 
-	
-	public String renderOnChangeEvent(UIForm uiForm) throws Exception {
-		if(componentEvent_ == null)	return uiForm.event(onchange_, null);
-		return	uiForm.event(onchange_, componentEvent_ , (String)null);
-	}
-	
-	final public boolean isChecked() { return checked; }	
-	
-	@SuppressWarnings("unchecked")
-	final public UIForumCheckBoxInput setChecked(boolean check) { 
+	final public UIForumCheckBoxInput setCheckedBox(boolean check) { 
 		checked = check;
+		setChecked(check);
 		return this ;
 	} 
 	
-	public void decode(Object input, WebuiRequestContext context)	throws Exception {
-		if (!isEnable()) return ;		
-		if (input == null) checked = false; else checked = true;
-		if(typeValue_ == Boolean.class || typeValue_ == boolean.class) {
-			value_ = typeValue_.cast(checked);
-		}
-	}
-	
-	public void processRender(WebuiRequestContext context) throws Exception {
-		Writer w = context.getWriter() ;		
-		w.write("<input type='checkbox' name='"); w.write(name); w.write("'") ;
-		w.write(" value='"); 
-		if(value_ != null)	w.write(String.valueOf(value_));
-		w.write("' ");
-		if(onchange_ != null) {
-			UIForm uiForm = getAncestorOfType(UIForm.class) ;
-			w.append(" onclick=\"").append(renderOnChangeEvent(uiForm)).append("\"");
-		}
-		if(checked) w.write(" checked ") ;
-		if (!enable_)	w.write(" disabled ");		
-		w.write(" class='checkbox'/> ") ;
-		w.write(name + "<br/>") ;
-	}
+	public void processRender(WebuiRequestContext context) throws Exception
+  {
+     Writer w = context.getWriter();
+     w.write("<input type='checkbox' name='");
+     w.write(name);
+     w.write("'");
+     w.write(" value='");
+     if (value_ != null)
+        w.write(String.valueOf(value_));
+     w.write("' ");
+     if (isChecked())
+        w.write(" checked ");
+     if (!enable_)
+        w.write(" disabled ");
+     w.write(" class='checkbox'/>");
+     w.write("<span> " + name + "</span><br/>") ;
+     if (this.isMandatory())
+        w.write(" *");
+  }
 }
