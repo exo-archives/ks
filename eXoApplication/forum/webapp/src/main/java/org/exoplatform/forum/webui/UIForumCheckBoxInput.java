48ca3709cc405244080e38056fc877bbc7865192
Huong Thien Nguyen <huongnt@exoplatform.com>
2011-05-23 10:21:51 +0000

    KS-3196: Poll portlet header not translated to English or French - UIPollForm
        * Add new key and add new title for UIPollForm.
        * Add new key for UIFormCheckBoxInput and UIFormMultiValueInputSet for Add Item and Remove Item.

--- eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/UIForumCheckBoxInput.java
+++ eXoApplication/forum/webapp/src/main/java/org/exoplatform/forum/webui/UIForumCheckBoxInput.java
@@ -8,7 +8,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -19,90 +19,74 @@ package org.exoplatform.forum.webui;
 import java.io.Writer;
 
 import org.exoplatform.webui.application.WebuiRequestContext;
-import org.exoplatform.webui.form.UIForm;
+import org.exoplatform.webui.form.UIFormCheckBoxInput;
 import org.exoplatform.webui.form.UIFormInput;
-import org.exoplatform.webui.form.UIFormInputBase;
 
 /**
  * Created by The eXo Platform SAS
  * Author : Vu Duy Tu
- *          tu.duy@exoplatform.com
+ *					tu.duy@exoplatform.com
  * 24 Mar 2008, 08:00:59
  */
-public class UIForumCheckBoxInput<T> extends UIFormInputBase<T>{
-	 /**
-   * Whether this checkbox is checked
-   */
+public class UIForumCheckBoxInput<T> extends UIFormCheckBoxInput<T> {
+  /**
+  * Whether this checkbox is checked
+  */
   private boolean checked = false;
+
   /**
    * A javascript expression that will be fired when the value changes (JS onChange event)
    */
-  private String onchange_;
-  private String componentEvent_ = null;
 
   @SuppressWarnings("unchecked")
   public UIForumCheckBoxInput(String name, String bindingExpression, T value) {
     super(name, bindingExpression, null);
-    if(value != null) typeValue_ = (Class<T>)value.getClass();
+    if (value != null)
+      typeValue_ = (Class<T>) value.getClass();
     value_ = value;
     setId(name);
   }
-  
-  @SuppressWarnings("unchecked")
-  public UIFormInput setValue(T value){
-    if(value == null) return super.setValue(value);
-    if(value instanceof Boolean){
-      checked = ((Boolean)value).booleanValue();
-    } else if(boolean.class.isInstance(value)){
+
+  public UIFormInput setValue(T value) {
+    if (value == null)
+      return super.setValue(value);
+    if (value instanceof Boolean) {
+      checked = ((Boolean) value).booleanValue();
+    } else if (boolean.class.isInstance(value)) {
       checked = boolean.class.cast(value);
     }
-    typeValue_ = (Class<T>)value.getClass();
+    typeValue_ = (Class<T>) value.getClass();
     return super.setValue(value);
   }
-  
-  public void setOnChange(String onchange){ onchange_ = onchange; }  
- 
-  public void setComponentEvent(String com){ componentEvent_ = com; }
-  
-  public void setOnChange(String event, String com){
-    this.onchange_ = event; 
-    this.componentEvent_ = com;
-  } 
-  
-  public String renderOnChangeEvent(UIForm uiForm) throws Exception {
-    if(componentEvent_ == null)  return uiForm.event(onchange_, null);
-    return  uiForm.event(onchange_, componentEvent_ , (String)null);
+
+  final public boolean isCheckedBox() {
+    checked = isChecked();
+    return checked;
   }
-  
-  final public boolean isChecked() { return checked; }  
-  
-  @SuppressWarnings("unchecked")
-  final public UIForumCheckBoxInput setChecked(boolean check) { 
+
+  final public UIForumCheckBoxInput setCheckedBox(boolean check) {
     checked = check;
-    return this ;
-  } 
-  
-  public void decode(Object input, WebuiRequestContext context)  throws Exception {
-    if (!isEnable()) return ;    
-    if (input == null) checked = false; else checked = true;
-    if(typeValue_ == Boolean.class || typeValue_ == boolean.class) {
-      value_ = typeValue_.cast(checked);
-    }
+    setChecked(check);
+    return this;
   }
-  
+
   public void processRender(WebuiRequestContext context) throws Exception {
-    Writer w =  context.getWriter() ;    
-    w.write("<input type='checkbox' name='"); w.write(name); w.write("'") ;
-    w.write(" value='"); 
-    if(value_ != null)  w.write(String.valueOf(value_));
+    Writer w = context.getWriter();
+    w.write("<input type='checkbox' name='");
+    w.write(name);
+    w.write("'");
+    w.write(" value='");
+    if (value_ != null)
+      w.write(String.valueOf(value_));
     w.write("' ");
-    if(onchange_ != null) {
-      UIForm uiForm = getAncestorOfType(UIForm.class) ;
-      w.append(" onclick=\"").append(renderOnChangeEvent(uiForm)).append("\"");
-    }
-    if(checked) w.write(" checked ") ;
-    if (!enable_)  w.write(" disabled ");    
-    w.write(" class='checkbox'/> ") ;
-    w.write(name + "<br/>") ;
+    if (isChecked())
+      w.write(" checked ");
+    if (!enable_)
+      w.write(" disabled ");
+    w.write(" class='checkbox'/>");
+    w.write("<span> " + name + "</span><br/>");
+    if (this.isMandatory())
+      w.write(" *");
   }
 }
+
