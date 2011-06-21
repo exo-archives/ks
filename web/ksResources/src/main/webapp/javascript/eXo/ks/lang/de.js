c9a7d087279e9d7b62ab6399d0ecabd4d498de96
Trang Huyen Vu <trangvh@exoplatform.com>
2011-06-21 03:46:34 +0000

    KS-3177: add lang folder, update patch for mini-calendar in Forums/Advanced search

---
+++ web/ksResources/src/main/webapp/javascript/eXo/ks/lang/de.js
@@ -0,0 +1,7 @@
+// ISO standard language abbreviation
+[
+	[ 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+	['S','M','D','M','D','F','S'],
+	[ 'Vorjahr', 'Vorhergehender Monat', 'Next Month', 'Nächstes Jahr']
+]
+
---
+++ web/ksResources/src/main/webapp/javascript/eXo/ks/lang/fr.js
@@ -0,0 +1,6 @@
+// ISO standard language abbreviation
+[
+ ['janvier','février','mars','avril ','mai','juin','juillet','août','septembre','octobre','novembre','décembre'],
+ ['D', 'L', 'M', 'M', 'J', 'V', 'S'], // Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday  
+ ['Année précédente', 'Mois précédent', 'Mois prochain', 'L\'année prochaine']
+]
