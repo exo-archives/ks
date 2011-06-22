0e18f0d37fbd21eea25b299ea20076c8005bcb50
Tu Duy Vu <tuvd@exoplatform.com>
2011-06-22 11:17:46 +0000

    KS-3383:  [wiki] Scroll Management improvements

--- eXoApplication/wiki/webapp/src/main/webapp/javascript/eXo/wiki/spliter/TableSpliter.js
+++ eXoApplication/wiki/webapp/src/main/webapp/javascript/eXo/wiki/spliter/TableSpliter.js
@@ -16,45 +16,102 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-
-if (!eXo.wiki)
+if (!eXo.wiki) {
   eXo.wiki = {};
-
-function TableSpliter() {
 };
 
-TableSpliter.prototype.exeRowSplit = function(e, markerobj) {  
-  _e = (window.event) ? window.event : e;
-  this.posX = _e.clientX;
-  this.posY = _e.clientY;
-  var marker = (typeof (markerobj) == "string") ? document
-      .getElementById(markerobj) : markerobj;
-  this.beforeArea = eXo.core.DOMUtil.findPreviousElementByTagName(marker, "td");
-  this.afterArea = eXo.core.DOMUtil.findNextElementByTagName(marker, "td"); 
-  if (this.beforeArea && this.afterArea && this.beforeArea.style.display !="none" && this.afterArea.style.display !="none") {
-    if (marker.offsetWidth >= marker.offsetHeight) {
-      this.adjustVertical();
-    } else {
-      this.adjustHorizon();
-    }
+function TableSpliter() {
+  this.posX = 0;
+  this.posY = 0;
+};
+
+window.onresize = function(){
+   eXo.wiki.TableSpliter.processeWithHeight();
+};
+
+
+TableSpliter.prototype.init = function(formId) {
+  var form = document.getElementById(formId);
+  var DOMUtil = eXo.core.DOMUtil;
+  this.portal = document.getElementById('UIPortalApplication'); 
+  document.body.style.overflowY = 'hidden';
+  this.layoutContainer = DOMUtil.findFirstDescendantByClass(form,"div", "WikiLayout") ;
+  this.spliter = DOMUtil.findFirstDescendantByClass(form,"div", "Spliter") ;
+  this.leftArea = DOMUtil.findPreviousElementByTagName(this.spliter, "div");
+  this.rightArea = DOMUtil.findNextElementByTagName(this.spliter, "div");
+  this.spliter.onmousedown = eXo.wiki.TableSpliter.exeRowSplit;
+  eXo.wiki.TableSpliter.processeWithHeight();
+};
+
+TableSpliter.prototype.processeWithHeight = function() {
+  var TableSpliter = eXo.wiki.TableSpliter ;
+  if(TableSpliter.layoutContainer) {
+    TableSpliter.setWithLayOut();
+    TableSpliter.setHeightLayOut();
+  } else {
+    TableSpliter.init();
   }
 };
 
-TableSpliter.prototype.adjustHorizon = function() {
-  this.beforeArea.style.width = this.beforeArea.offsetWidth + "px";
-  this.afterArea.style.width = this.afterArea.offsetWidth + "px";
-  this.beforeX = this.beforeArea.offsetWidth;
-  this.afterX = this.afterArea.offsetWidth;
-  document.onmousemove = eXo.wiki.TableSpliter.adjustWidth;
-  document.onmouseup = eXo.wiki.TableSpliter.clear;
-} ;
+TableSpliter.prototype.setWithLayOut = function() {
+  var TableSpliter = eXo.wiki.TableSpliter ;
+  var maxWith = TableSpliter.layoutContainer.offsetWidth;
+  var lWith = 0;
+  if(TableSpliter.leftArea && TableSpliter.spliter) {
+    lWith = TableSpliter.leftArea.offsetWidth + TableSpliter.spliter.offsetWidth;
+  }
+  TableSpliter.rightArea.style.width = (maxWith - lWith) + 'px';
+  TableSpliter.rightArea.style.left = lWith + 'px' ;
+};
 
-TableSpliter.prototype.adjustVertical = function() {
-  this.beforeArea.style.height = this.beforeArea.offsetHeight + "px";
-  this.afterArea.style.height = this.afterArea.offsetHeight + "px";
-  this.beforeX = this.beforeArea.offsetHeight;
-  this.afterX = this.afterArea.offsetHeight;
-  document.onmousemove = eXo.wiki.TableSpliter.adjustHeight;
+
+TableSpliter.prototype.setHeightLayOut = function() {
+  var TableSpliter = eXo.wiki.TableSpliter ;
+  var layout = eXo.wiki.TableSpliter.layoutContainer;
+  var hdef = document.documentElement.clientHeight - layout.offsetTop;
+  hdef = (hdef > 200)?hdef:200;
+  var hct = 0;
+  layout.style.height = hdef + 'px';
+  var delta = TableSpliter.heightDelta();
+  while (TableSpliter.heightDelta() > 0 && hdef >= 200) {
+    hct = hdef - delta;
+    hdef = hdef - 2;
+    layout.style.height = hct + 'px';
+  }
+  if(TableSpliter.leftArea && TableSpliter.spliter){
+    TableSpliter.leftArea.style.height = hct + "px";
+    TableSpliter.spliter.style.height = hct + "px";
+  }
+  TableSpliter.rightArea.style.height = hct + "px";
+  TableSpliter.setHeightRightContent();
+};
+
+TableSpliter.prototype.setHeightRightContent = function() {
+  var pageArea = document.getElementById("UIWikiPageArea");
+  var bottomArea = document.getElementById("UIWikiBottomArea");
+  if(pageArea && bottomArea){
+    pageArea.style.height = "auto";
+    var h = eXo.wiki.TableSpliter.layoutContainer.offsetHeight - (bottomArea.offsetHeight+15);
+    var x = pageArea.offsetHeight;
+    pageArea.style.height = ((h < x)?x:h) + "px";
+  }
+};
+
+TableSpliter.prototype.exeRowSplit = function(e) {  
+  _e = (window.event) ? window.event : e;
+  var TableSpliter = eXo.wiki.TableSpliter ;
+  TableSpliter.posX = _e.clientX;
+  TableSpliter.posY = _e.clientY;
+  if (TableSpliter.leftArea && TableSpliter.rightArea && TableSpliter.leftArea.style.display !="none" && TableSpliter.rightArea.style.display !="none") {
+      TableSpliter.adjustHorizon();
+  }
+};
+
+
+TableSpliter.prototype.adjustHorizon = function() {
+  this.leftX = this.leftArea.offsetWidth;
+  this.rightX = this.rightArea.offsetWidth;
+  document.onmousemove = eXo.wiki.TableSpliter.adjustWidth;
   document.onmouseup = eXo.wiki.TableSpliter.clear;
 } ;
 
@@ -62,26 +119,25 @@ TableSpliter.prototype.adjustWidth = function(evt) {
   evt = (window.event) ? window.event : evt ;
   var TableSpliter = eXo.wiki.TableSpliter ;
   var delta = evt.clientX - TableSpliter.posX ;
-  var afterWidth = (TableSpliter.afterX - delta) ;
-  var beforeWidth = (TableSpliter.beforeX + delta) ;
-  if (beforeWidth <= 0  || afterWidth <= 0) return ;
-  TableSpliter.beforeArea.style.width =  beforeWidth + "px" ;
-  TableSpliter.afterArea.style.width =  afterWidth + "px" ;  
+  var leftWidth = (TableSpliter.leftX + delta) ;
+  var rightWidth = (TableSpliter.rightX - delta) ;
+  if (rightWidth <= 0  || leftWidth <= 0) return ;
+  TableSpliter.leftArea.style.width =  leftWidth + "px" ;
+  TableSpliter.rightArea.style.width =  rightWidth + "px" ;  
+
+  TableSpliter.spliter.style.left =  leftWidth + "px" ;
+  TableSpliter.rightArea.style.left =  (leftWidth + TableSpliter.spliter.offsetWidth) + "px" ;  
 } ;
 
-TableSpliter.prototype.adjustHeight = function(evt) {
-  evt = (window.event) ? window.event : evt ;
-  var TableSpliter = eXo.wiki.TableSpliter ;
-  var delta = evt.clientY - TableSpliter.posY ;
-  var afterHeight = (TableSpliter.afterY - delta) ;
-  var beforeHeight = (TableSpliter.beforeY + delta) ;
-  if (beforeHeight <= 0  || afterHeight <= 0) return ;
-  TableSpliter.beforeArea.style.height =  beforeHeight + "px" ;
-  TableSpliter.afterArea.style.height =  afterHeight + "px" ;  
-} ;
 
 TableSpliter.prototype.clear = function() {  
   document.onmousemove = null ;  
 } ;
+
+
+TableSpliter.prototype.heightDelta = function() {
+  return this.portal.offsetHeight - document.documentElement.clientHeight;
+} ;
+
 
 eXo.wiki.TableSpliter = new TableSpliter() ;
